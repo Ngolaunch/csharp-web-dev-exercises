f6453025aa09e26e9fc780beee3a1b38b70e4e68
ngohile <ngohile.yakubu@gmail.com>
2023-03-08 21:53:53 -0600

    array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,5 +2,12 @@
 
 //TODO: Write your code for Array Practice below:
 
-Console.WriteLine("Hello, Arrays!");
+int[] arrayPractice = { 1, 1, 2, 3, 5, 8 } ;
+
+//Console.WriteLine(arrayPractice);
+
+foreach (int i in arrayPractice)
+{
+    Console.WriteLine(arrayPractice);
+}
 
