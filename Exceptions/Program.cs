0c68f6e73df4ccf5c0b37b78c0f7f15c477a04c1
ngohile <ngohile.yakubu@gmail.com>
2023-03-22 14:55:27 -0500

    exc.

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,23 +1,49 @@
 ﻿// --------- Divide By Zero! ----------
 
-/* 
+ 
 static double Divide(double x, double y)
 {
     // Write your code here!
+
+    try
+    {
+        return x / y;
+    } catch (ArgumentOutOfRangeException e)
+    { 
+        throw new ArgumentOutOfRangeException("Not divisible by zero");
+    }
+    
+    return x / y;
+
 }
-*/
+
 
 // Test out your Divide() function here!
+Console.WriteLine(Divide(7, 0));
+
+// Test out your Divide() function here!
+//Console.WriteLine(Divide(7, 0));
 
 
 // --------- Test Student Labs ----------
 
-/*
 static int CheckFileExtension(string fileName)
 {
     // Write your code here!
+    if (fileName == null || fileName == "")
+    {
+        throw new ArgumentNullException("fileName", "Student did not submit work.");
+    }
+
+    if (fileName.EndsWith(".cs"))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-*/
 
 // Test out your CheckFileExtension() function here!
 
@@ -25,6 +51,8 @@ Dictionary<string, string> students = new Dictionary<string, string>();
 students.Add("Carl", "Program.cs");
 students.Add("Brad", "");
 students.Add("Elizabeth", "MyCode.cs");
+
+Console.WriteLine(CheckFileExtension(students["Brad"]));
 
 
 
