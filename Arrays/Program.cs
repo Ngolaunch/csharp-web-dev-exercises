f55f3ba5f5f18cfd0cc48daec12d2a1910c2c4c7
ngohile <ngohile.yakubu@gmail.com>
2023-03-22 15:11:26 -0500

    added loop

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,8 +6,18 @@ int[] arrayPractice = { 1, 1, 2, 3, 5, 8 } ;
 
 //Console.WriteLine(arrayPractice);
 
-foreach (int i in arrayPractice)
-{
-    Console.WriteLine(arrayPractice[i]);
-}
+//foreach (int i in arrayPractice)
+//{
+//    Console.WriteLine(arrayPractice[i]);
+//}
 
+// ***you can only run one foreach at a time****
+
+
+foreach (int num in arrayPractice)
+{
+    if (num % 2 > 0)
+    {
+        Console.WriteLine(num);
+    }
+}
